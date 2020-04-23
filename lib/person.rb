543ce0b1c8bd58684f1318787a546ae5c99664dd
Jashema Panapa <jcpanapa@gmail.com>
2020-04-23 20:21:27 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,13 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
 :weight
   
-  def initialize(name:, birthday:, hair_color:)
+  def initialize(name:, birthday:, hair_color:, eye_color:, height:, weight:)
+    @name = name 
+    @birthday = birthday 
+    @hair_color = hair_color
+    @eye_color = eye_color
+    @height = height
+    @weight = weight
     
   end 
   
